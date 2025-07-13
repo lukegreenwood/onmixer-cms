518ebc800168b331bd82cd35b43a29889e63c999
Luke Greenwood <lukeyg101@gmail.com>
2025-07-13 21:07:21 +0100

    fix: update link

--- src/app/auth/AuthContent.tsx
+++ src/app/auth/AuthContent.tsx
@@ -3,8 +3,6 @@
 import { Alert, Button } from '@soundwaves/components';
 import { useRouter } from 'next/navigation';
 
-import { AUTH_CONFIG } from '@/lib/auth-config';
-
 export const AuthContent = ({
   errorMessage,
 }: {
@@ -13,15 +11,8 @@ export const AuthContent = ({
   const router = useRouter();
 
   const handleSignIn = () => {
-    const authUrl = new URL(AUTH_CONFIG.BASE_URL);
-    const redirectUri = `${window.location.origin}/auth/callback`;
-
-    const signInUrl = `${authUrl}/login?${new URLSearchParams({
-      client_id: AUTH_CONFIG.CLIENT_ID,
-      redirect_uri: redirectUri,
-    }).toString()}`;
-
-    router.push(signInUrl);
+    // going back to home page will trigger the middleware to redirect to auth
+    router.push(`/`);
   };
   return (
     <>
